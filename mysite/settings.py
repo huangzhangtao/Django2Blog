41d95794cf038788d51996ca6aa846adb56491bf
yzy <huangzhangtao24@163.com>
2019-04-16 20:32:01 -0700

    revise

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,14 +82,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-'''
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
